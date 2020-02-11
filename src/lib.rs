4c8f3cce2ca28fdc965dd8e43a1aa2009199faf7
Jimmy Chu <jimmychu0807@gmail.com>
2020-02-11 17:51:02 +0800

    Splitting tests out

--- src/lib.rs
+++ src/lib.rs
@@ -8,52 +8,9 @@ pub mod impls;
 pub mod json;
 pub mod parser;
 
-pub fn parse_json(input: &str) -> Result<JsonValue, SimpleError> {
-    Json::parse(&input, Default::default()).map(|(ret, _)| ret)
-}
-
 #[cfg(test)]
-mod tests {
-  use super::*;
+mod tests;
 
-  extern crate alloc;
-  use alloc::vec;
-  use crate::json::NumberValue;
-
-  #[test]
-  fn lib_main() {
-    assert_eq!(
-      parse_json(&r#"{ "test": 1, "test2": [1e-4, 2.041e2, true, false, null, "\"1\n\""] }"#)
-        .map_err(|_| ()),
-      Ok(JsonValue::Object(vec![
-        (vec!['t', 'e', 's', 't'], JsonValue::Number(NumberValue {
-          integer: 1,
-          fraction: 0,
-          fraction_length: 0,
-          exponent: 0
-        })),
-        (
-          vec!['t', 'e', 's', 't', '2'],
-          JsonValue::Array(vec![
-            JsonValue::Number(NumberValue {
-              integer: 1,
-              fraction: 0,
-              fraction_length: 0,
-              exponent: -4,
-            }),
-            JsonValue::Number(NumberValue {
-              integer: 2,
-              fraction: 41,
-              fraction_length: 3,
-              exponent: 2,
-            }),
-            JsonValue::Boolean(true),
-            JsonValue::Boolean(false),
-            JsonValue::Null,
-            JsonValue::String(vec!['\"', '1', 'n', '\"'])
-          ])
-        )
-      ]))
-    )
-  }
+pub fn parse_json(input: &str) -> Result<JsonValue, SimpleError> {
+  Json::parse(&input, Default::default()).map(|(ret, _)| ret)
 }
